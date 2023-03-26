f885cfb11cf6ed992e41bc643a5b6d380f6b320a
Your Name <lisleclara1@gmail.com>
2023-03-26 09:48:18 -0300

    typescript

---
+++ ejemplos.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var semana;
+(function (semana) {
+    semana[semana["lunes"] = 1] = "lunes";
+    semana[semana["martes"] = 2] = "martes";
+    semana[semana["miercoles"] = 3] = "miercoles";
+})(semana || (semana = {}));
+console.log(semana.lunes);
+console.log(semana.miercoles);
---
+++ ejemplos.ts
@@ -0,0 +1,10 @@
+enum semana {
+    lunes =1,
+    martes, 
+    miercoles
+}
+
+console.log(semana.lunes);
+console.log(semana.miercoles);
+
+export{}
